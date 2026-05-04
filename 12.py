f24dbd66e0920752686e5c638a36d77b32e31dbc
kuldeep sharma <mekuldeep03@gmail.com>
2026-05-04 23:55:26 +0530

    solve problem 12

---
+++ 12.py
@@ -0,0 +1,18 @@
+def romanizer(numbers):
+    values = [
+        (1000, 'M'), (900, 'CM'), (500, 'D'), (400, 'CD'),
+        (100, 'C'), (90, 'XC'), (50, 'L'), (40, 'XL'),
+        (10, 'X'), (9, 'IX'), (5, 'V'), (4, 'IV'), (1, 'I')
+    ]
+    def convert(n):
+        result = ""
+        for val, symbol in values:
+            while n >= val:
+                result += symbol
+                n -= val
+        return result
+    return [convert(n) for n in numbers]
+
+
+result = romanizer([75,80,99,100, 875])
+print(result)
